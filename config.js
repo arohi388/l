c0c30bde10dd974e8ae4b05164c5bfc71f089df8
arohi388 <61878380+arohi388@users.noreply.github.com>
2021-02-05 05:21:28 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyD4XVUsoBQpm_v4a8KBsGpLT0AkEwprr0o",
+    authDomain: "library-d8033.firebaseapp.com",
+    projectId: "library-d8033",
+    storageBucket: "library-d8033.appspot.com",
+    messagingSenderId: "668339336346",
+    appId: "1:668339336346:web:87ea30dca1491b83059b23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+export default firebase.firestore()
